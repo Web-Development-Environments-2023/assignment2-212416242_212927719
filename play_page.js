9077920e1ce8fcc2e569646d5194d37158d23323
TevaErez <104378468+TevaErez@users.noreply.github.com>
2023-04-14 12:50:58 +0300

    spaceshipsAdded

--- play_page.js
+++ play_page.js
@@ -1,56 +1,92 @@
-var spaceShip = new Object;
+var GoodSpaceship = new Object;
 var c = document.getElementById("MyCanvas");
-var img = new Image();
-var img_height = c.height/10;
-var img_width = c.width/10;
+var ctx = c.getContext("2d");
+var intervalGap = 125;
+var GoodSSImg = new Image();
+GoodSSImg.src = "goodSpaceship1.png";
+var BadSSImg = new Image();
+BadSSImg.src = 'badSpaceship.png';
+var img_height = c.height/15; 
+var img_width = c.width/15;
 var shoot = null;
 var keyDown;
+var badspaceShips;
+var spaceshipsMovement = "right";
+
+function initiateBadspaceShipsLocations(firstSpacehipI){
+    badspaceShips[0][0].i=firstSpacehipI;
+    badspaceShips[0][0].j=img_height;
+    for (var i = 0; i < badspaceShips.length; i++) {
+        var j = 0;
+        if (i==0){
+            j=1;
+        }
+        for (; j < badspaceShips[i].length; j++) {
+            badspaceShips[i][j].alive = true;
+            badspaceShips[i][j].i=badspaceShips[0][0].i + img_width * i * 2;
+            badspaceShips[i][j].j=badspaceShips[0][0].j + img_height * j * 2;
+        
+        }
+    }
+}
 
 
 function Start() {
-    spaceShip.i=c.width/2-img_width/2;
-    spaceShip.j=c.height - c.height * 0.2 - img_height/2;
+    function createBadspaceShips(){
+        function initiateObjects(){
+            const numRows = 5;
+            const numCols = 4;
+            badspaceShips = new Array(numRows);
+            for (let i = 0; i < numRows; i++) {
+                badspaceShips[i] = new Array(numCols);  
+              for (let j = 0; j < numCols; j++) {
+                badspaceShips[i][j] = new Object();
+              }
+            }
+        }    
+        initiateObjects();
+        initiateBadspaceShipsLocations(0);
+    }
+    createBadspaceShips();
+    GoodSpaceship.i=c.width/2-img_width/2;
+    GoodSpaceship.j=c.height - c.height * 0.2 - img_height/2;
     addEventListener("keydown", function (e) {
         keyDown = e.key;}, false);
-    addEventListener("keyup", function (e) {
-        console.log(e.key);
-        keyDown = null;}, false);
-    interval=setInterval(UpdatePosition, 250);
+
+    interval=setInterval(UpdatePosition, intervalGap);
 }
 
 
 function UpdatePosition() {
-
     var jump_size_vertical = img_height/2;
     var jump_size_horizontal = img_width/2;
-
     if(keyDown=="ArrowUp")
     {
 
-        if(spaceShip.j>c.height - c.height * 0.4 + jump_size_vertical/2)
+        if(GoodSpaceship.j>c.height - c.height * 0.4 + jump_size_vertical/2)
         {
-            spaceShip.j-=jump_size_vertical;
+            GoodSpaceship.j-=jump_size_vertical;
         }
     }
     if(keyDown=="ArrowDown")
     {
-        if(spaceShip.j<c.height - img_height - jump_size_vertical/2)
+        if(GoodSpaceship.j<c.height - img_height - jump_size_vertical/2)
         {
-            spaceShip.j+=jump_size_vertical;
+            GoodSpaceship.j+=jump_size_vertical;
         }
     }
     if(keyDown=="ArrowLeft")
     {
-        if(spaceShip.i>0)
+        if(GoodSpaceship.i>0)
         {
-            spaceShip.i-=jump_size_horizontal;
+            GoodSpaceship.i-=jump_size_horizontal;
         }
     }
     if(keyDown=="ArrowRight")
     {
-        if(spaceShip.i<c.width - img_width)
+        if(GoodSpaceship.i<c.width - img_width)
         {
-            spaceShip.i+=jump_size_horizontal;
+            GoodSpaceship.i+=jump_size_horizontal;
         }
     }
     if(keyDown=="Escape"){
@@ -59,27 +95,30 @@ function UpdatePosition() {
 
     }
     if(keyDown==shoot){
-        window.clearInterval(interval);
-        $("#MyCanvas").hide()
+        //
 
     }
     else{
+        if(badspaceShips[0][0].i<img_width*6 && spaceshipsMovement == "right"){
+            initiateBadspaceShipsLocations(badspaceShips[0][0].i+img_width/2,0);
+        }
+        else{
+            spaceshipsMovement = "left";
+            if(badspaceShips[0][0].i!=0){
+                initiateBadspaceShipsLocations(badspaceShips[0][0].i-img_width/2);
+            }
+            else{
+                initiateBadspaceShipsLocations(badspaceShips[0][0].i+img_width/2);
+                spaceshipsMovement = "right"
+            }
+        }
         Draw();
     }
+    keyDown=null;
 }
 
-function Draw() {
-    DrawGoodRocketShip();
-    
-}
-
-function DrawGoodRocketShip(){
-    $(img).on('load', function() {
-        ctx.clearRect(0, 0, c.width, c.height);
-    });
-    var ctx = c.getContext("2d");
-    img.src = 'spaceship1.png';
-    $(img).on('load', function() {
+function Draw(){
+    function draw_line(){
         ctx.strokeStyle = "black"; 
         var gradient = ctx.createLinearGradient(0, 0, c.width, 0);
         gradient.addColorStop(0, "black");
@@ -89,14 +128,29 @@ function DrawGoodRocketShip(){
         ctx.moveTo(0, c.height*0.6-1);
         ctx.lineTo(c.width, c.height*0.6-1);
         ctx.stroke();
+    }
+    function draw_text(){
         ctx.font = "10px fantasy";
         ctx.fillStyle="white"
         ctx.fillText("can't cross", 0, c.height*0.6 - 2);
-        ctx.drawImage(img, spaceShip.i, spaceShip.j, img_width,img_height);
-    });
+    }
+    function draw_badSpaceships(){
+        for (var i = 0; i < badspaceShips.length; i++) {
+            for (var j = 0; j < badspaceShips[i].length; j++) {
+                ctx.drawImage(BadSSImg, badspaceShips[i][j].i, badspaceShips[i][j].j, img_width,img_height);
+            }
+        }
+    
+    }
+
+    c.width=c.width;
+    draw_line();
+    draw_text();
+    ctx.drawImage(GoodSSImg, GoodSpaceship.i, GoodSpaceship.j, img_width,img_height);
+    draw_badSpaceships();
 }
 
-$("#MyCanvas").hide()
+$("#MyCanvas").hide();
 const input = document.getElementById("myInput");
 input.addEventListener('keydown',function(e) {
   if (input.value.length > 0) {
@@ -113,6 +167,7 @@ input.addEventListener('keydown',function(e) {
 const myButton = document.getElementById("startButton")
 startButton.addEventListener("click",function(){
     if(shoot==null){
+        $("#mytextarea").empty();
         $("#mytextarea").append(" *fill the box");
     }
     else{
@@ -120,4 +175,3 @@ startButton.addEventListener("click",function(){
         $(document).ready(Start);
     }
 }, false);
-  
