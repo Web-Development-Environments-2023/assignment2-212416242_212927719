cb5739bc9d46c3b1b2d94c45e5d5336528af516a
Omer Aflalo <omer.aflalo@gmail.com>
2023-04-19 17:39:36 +0300

    Add signup functionality

---
+++ signup.js
@@ -0,0 +1,26 @@
+const users = {};
+var loggedInUser;
+
+
+
+
+function submitSignUp() {
+    // const errorMsg = document.getElementById('error-signup');
+    let confirm_password = document.getElementById('confirmPassword').value;
+    let password = document.getElementById('password').value;
+    let username = document.getElementById('userName').value;
+    let firstName = document.getElementById('firstName').value;
+    let lastName = document.getElementById('lastName').value;
+    let email = document.getElementById('email').value;
+    let date = document.getElementById('date').value;
+    if (password === confirm_password) {
+        const newUser = { username: username, password: password, firstName: firstName, lastName: lastName, email: email, date: date };
+        users[username] = newUser;
+        console.log(users);
+        goToHomePage()
+    }
+    else {
+        // errorMsg.style.display = 'block';
+    }
+}
+
