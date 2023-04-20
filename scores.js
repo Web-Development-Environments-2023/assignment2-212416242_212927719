601c1ae0c17e2dfa99e2134b84c04eb359762dfb
Omer Aflalo <omer.aflalo@gmail.com>
2023-04-20 03:26:51 +0300

    Add score script that updates the table

---
+++ scores.js
@@ -0,0 +1,16 @@
+function updateScoreTable() {
+    $("#scoresData td").remove();
+    var table = document.getElementById('scoresData');
+    for (const dataRow of UserScores) {
+        var row = table.insertRow(0);
+        var Date = row.insertCell(0);
+        var time = row.insertCell(1);
+        var score = row.insertCell(2);
+        var lives = row.insertCell(3);
+        Date.innerHTML = dataRow.date;
+        time.innerHTML = dataRow.time;
+        score.innerHTML = dataRow.score;
+        lives.innerHTML = dataRow.lives;
+
+    }
+}
