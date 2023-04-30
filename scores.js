709d525176c34a0398f91f4e58ed998283c990f4
omer aflalo <omer.aflalo@gmail.com>
2023-05-01 00:48:42 +0300

    update score sort

--- scores.js
+++ scores.js
@@ -1,17 +1,23 @@
 function updateScoreTable() {
     $("#scoresData td").remove();
     var table = document.getElementById('scoresData');
-    
-    for (const dataRow of UserScores) {
+    let sortedScore=UserScores.sort(function(a, b)
+    {
+     return a.score>b.score;
+    });
+    let i=1;
+    for (const dataRow of sortedScore) {
         var row = table.insertRow(0);
-        var Date = row.insertCell(0);
-        var score = row.insertCell(1);
-        var time = row.insertCell(2);
-        var lives = row.insertCell(3);
+        var Rank = row.insertCell(0);
+        var Date = row.insertCell(1);
+        var score = row.insertCell(2);
+        var time = row.insertCell(3);
+        var lives = row.insertCell(4);
+        Rank.innerHTML = i;
         Date.innerHTML = dataRow.date;
         time.innerHTML = dataRow.time + "s";
         score.innerHTML = dataRow.score;
         lives.innerHTML = dataRow.lives;
-
+        i++;
     }
 }
